87a76451488b62e8ed27b05a7608d9192694cecb
Felipe Rivas <rivascf@gmail.com>
2021-10-15 11:21:24 -0500

    Updated main script

--- main.py
+++ main.py
@@ -53,4 +53,3 @@ if __name__ == '__main__':
     dronectrl.start_drone()
     print('Drone activado: bateria:{}, {}'.format(dronectrl.get_battery(), dronectrl.get_state()))
     dronectrl.start_camera()
-
